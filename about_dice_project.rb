9f60127168bb79fa7d7df1e5f98b3ed74a478129
Alexandre <mrtalexandre@gmail.com>
2021-05-11 00:10:46 -0300

    Add about dice project

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -6,6 +6,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+
+class DiceSet
+
+  def roll(number)
+    @values = Array.new(number) { rand(1..6) }
+  end
+
+  def values
+    @values
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
